ca7ac6c6d49f73327f1c3e6e3f96451aa2d4a874
Dylan Demougin <demougin.dylan@gmail.com>
2020-05-01 17:39:33 +0200

    #2 improve help command : use commands directly to generate automatically help

--- src/templates/Help.command.js
+++ src/templates/Help.command.js
@@ -2,20 +2,36 @@ const bot = require('../BotHelper').get()
 
 module.exports = {
     name: 'help',
+    help: 'Ce message',
     execute(message) {
+        // Search all commands
+        const fields = bot.commands
+            // remove command without help
+            .filter(command => command.help)
+            // put this help first in command
+            .sort((a, b) => {
+                if (a.name === this.name) {
+                    return -1
+                }
+                if (b.name === this.name) {
+                    return 1
+                }
+                return 0
+            })
+            // map command for generate embed field items
+            .map(command => ({
+                name: `${process.env.COMMAND_START}${command.name}`,
+                value: command.help
+            }))
+
         message.reply({
             embed: {
                 author: {
                     name: bot.user.username,
                     icon_url: bot.user.avatarURL
                 },
-                title: 'Commands list',
-                fields: [
-                    {
-                        name: `${process.env.COMMAND_START}help`,
-                        value: `Show this message`
-                    },
-                ],
+                title: 'Liste des commandes',
+                fields: fields,
                 timestamp: new Date(),
                 footer: {
                     icon_url: bot.user.avatarURL
